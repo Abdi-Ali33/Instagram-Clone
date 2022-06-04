042dd27122b2394c7edf88a15e3da2a020ed06d0
Abdi Ali <alikune33@gmail.com>
2022-06-04 16:37:03 +0300

    creating partials file inside templates

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from app.models import Follower, Image, Likes, Profile, User, Comments
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Follower)
+admin.site.register(Comments)
+admin.site.register(Likes)
+admin.site.register(Profile)
+admin.site.register(Image)
