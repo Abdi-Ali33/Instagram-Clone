c995d2ef289495f681ae1dc5ce2c871b19fea72c
Abdi Ali <alikune33@gmail.com>
2022-06-07 15:36:44 +0300

    making changes in model.py

--- app/models.py
+++ app/models.py
@@ -16,10 +16,12 @@ class User(AbstractUser):
     def url_formatted_name(self):
         return self.full_name.replace(' ', '+') or self.username
 
-    # @property
-    # def fallback_avatar(self):
-    #     return f'https://ui-avatars.com/api/?name={self.url_formatted_name}&background=8655ff&color=fff'
+    @property
+    def fallback_avatar(self):
+        return f'https://avatars.abstractapi.com/v1/?api_key=3257ee7097604595918679f1f6fc78a2&name=Claire Florentz'
+        # https: // ui-avatars.com/api /?name = {self.url_formatted_name} & background = 8655ff & color = fff
 
+# https://avatars.abstractapi.com/v1/?api_key=3257ee7097604595918679f1f6fc78a2&name=Claire Florentz
     @property
     def avatar(self):
         return self.profile.avatar
